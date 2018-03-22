f831d3551375cdff96cbcea42538f410c21682be
luispcunha <luispplmcunha@gmail.com>
2018-03-22 16:10:28 +0000

    added 3 functions

--- Prog_proj1/Prog_proj1.cpp
+++ Prog_proj1/Prog_proj1.cpp
@@ -5,6 +5,98 @@
 
 using namespace std;
 
+
+/**
+Checks if the string is a headline
+@param line: string to be evaluated
+@return value: true if string only contains uppercase letters ('A'-'Z') or the characters ' ' (space), ';' (semicolon), '-' (hyphen), or '\'' (apostrophe)
+*/
+bool isHeadline(string line)
+{
+	bool headline = true; // return value
+
+
+	for (int i = 0; i < line.size(); i++)
+	{
+		char currentChar = line.at(i);
+
+		if (isalpha(currentChar))
+		{
+			if (currentChar != toupper(currentChar)) // check if char is alphabetic lower case
+			{
+				headline = false; // if char is lower case, return value is false and end loop
+
+				break;
+			}
+		}
+		else if ((currentChar != ' ') && (currentChar != '\'') && (currentChar != '-') && (currentChar != ';')) // if not letter and different than valid non alphabetic chars
+		{
+			headline = false;
+
+			break;
+		}
+	}
+
+	return headline;
+}
+
+/**
+Checks if there's any non alphabetic char
+@param line: string to be evaluated
+@return value: true if word is simple and valid to add to main vector
+*/
+bool isSimpleWord(string line)
+{
+	bool simpleWord = true;
+
+	for (int i = 0; i < line.size(); i++)
+	{
+		char currentChar = line.at(i);
+
+		if (!isalpha(currentChar))
+		{
+			simpleWord = false;
+			break;
+		}
+	}
+
+	return simpleWord;
+}
+
+/**
+Checks if headline needs to be decomposed
+@param line: string to be evaluated
+@return value: true if word has semicolons(i.e. has multiple expressions/words) and needs to be decomposed
+*/
+bool hasMultipleWords(string line)
+{
+	bool hasSemicolon = false;
+
+	for (int i = 0; i < line.size(); i++)
+	{
+		char currentChar = line.at(i);
+
+		if (currentChar == ';')
+		{
+			hasSemicolon = true;
+
+			break;
+		}
+	}
+
+	return hasSemicolon;
+}
+
+/*
+[x] getline is working, all functions tested
+[] decompose function
+*/
+
+
+
+
+//------------------------------------------------------------------------------------------------------------
+
 /**
 
 */
@@ -28,8 +120,8 @@ void processLine(string line, vector<string>& wordVector)
 
 
 /**
-Reads the contents of the input File, line by line, calls the functions that process the line, and updates the counters(for output purposes).
-@param iFile address of the input file
+Reads the contents of the input File, line by line, calls the functions that process the line, and updates the counters(for output purposes)
+@param iFile: address of the input file
 */
 
 void readFile(ifstream& iFile)
