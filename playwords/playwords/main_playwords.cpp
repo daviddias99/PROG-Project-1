154865c3b8aa56f80681d186665af5a22c2d20b9
luispcunha <luispplmcunha@gmail.com>
2018-03-25 21:45:34 +0100

    Added first function
    .added function to see if a word belongs on the list
    .words.txt is the text file produced by program 1

--- playwords/playwords/main_playwords.cpp
+++ playwords/playwords/main_playwords.cpp
@@ -14,7 +14,7 @@ Asks for the name of the file and trys to open the file
 bool getWordFile(ifstream& file)
 {
 	string fileName;
-	
+
 	//Gets the name of the input file
 	cout << "Word file ? ";
 	cin >> fileName;
@@ -46,12 +46,74 @@ Reads word file saving all words into a vector of strings
 @param file: file to read
 @param words: vector where words are stored
 */
-void readFile(ifstream& file, vector<string>& words)
+void readFile(ifstream& file, vector<string>& wordVector)
 {
 	string word;
 
 	while (file >> word)
-		words.push_back(word);
+		wordVector.push_back(word);
+}
+
+
+/**
+Receives a string and returns it with all caps 
+@param word: word we want to make all caps
+@return value: same word, all caps
+*/
+string allCaps(string word)
+{
+	for (int i = 0; i < word.size(); i++)
+	{
+		word.at(i) = toupper(word.at(i));
+	}
+
+	return word;
+}
+
+
+/**
+Uses binary search to see if a user entered word matches a word on the vector
+@param wordVector: vector of words
+@param word: user entered word 
+@return value: true if the word matches a word on the vector, false if the word doesn't belong on the vector
+*/
+bool searchWord(vector<string> wordVector, string word)
+{
+	int lower = 0;
+	int upper = wordVector.size() - 1;
+	int mid;
+
+	while (upper >= lower)
+	{
+		mid = (upper + lower) / 2;
+
+		if (word == wordVector.at(mid))
+			return true;
+		else if (word > wordVector.at(mid))
+			lower = mid + 1;
+		else if (word < wordVector.at(mid))
+			upper = mid - 1;
+	}
+	
+	return false;
+}
+
+
+/**
+Asks the user for a word and checks if it belongs on the list
+@param wordvector: vector containing all words
+*/
+void wordExists(vector<string> wordVector)
+{
+	string inputWord;
+
+	cout << "Enter a word: "; cin >> inputWord;
+
+	if (searchWord(wordVector, allCaps(inputWord)))
+		cout << "The word " << inputWord << " belongs on the list." << endl;
+	else
+		cout << "The word " << inputWord << " doesn't belong on the list." << endl;
+
 }
 
 
@@ -61,16 +123,23 @@ void readFile(ifstream& file, vector<string>& words)
 int main()
 {
 	ifstream wordFile;
-	vector<string> words;
+	vector<string> wordVector;
+
 	
 	//Open word file
 	while (!getWordFile(wordFile)); 
 
 
 	//Save words into vector and close word file
-	cout << "Reading word file ... " << endl;
-	readFile(wordFile, words);
+	cout << endl << "Reading word file ... " << endl;
+	readFile(wordFile, wordVector);
 	wordFile.close();
+	cout << endl;
+
+
+	//Does the word belong on the list? **first function**
+	wordExists(wordVector);
+	
 
 
 
